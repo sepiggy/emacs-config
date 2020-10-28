65d5a9c119f8d5742b7654b4f75e0ef3ba799359
sepiggy <jms1209@qq.com>
2020-10-28 17:46:03 +0800

    Update UI settings

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -7,12 +7,6 @@
 	sml/theme 'respectful)
   (sml/setup))
 
-;; see https://stackoverflow.com/questions/294664/how-to-set-the-font-size-in-emacs
-(set-face-attribute 'default nil :font "Source Code Pro" :height 160)
-
-(setq display-line-numbers-type 'relative)
-(global-display-line-numbers-mode t)
-
 (use-package ivy-posframe
   :init
   (setq ivy-posframe-display-functions-alist
@@ -22,5 +16,14 @@
 	  (counsel-find-file . ivy-posframe-display-at-frame-center)
 	  (ivy-switch-buffer . ivy-posframe-display-at-frame-center)
 	  (t . ivy-posframe-display-at-frame-center))))
+
+(use-package ace-window
+  :bind (("M-o" . 'ace-window)))
+
+;; see https://stackoverflow.com/questions/294664/how-to-set-the-font-size-in-emacs
+(set-face-attribute 'default nil :font "Source Code Pro" :height 160)
+
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode t)
 
 (provide 'init-ui)
